c91d6322634631f2a8f635729ca2b178ffa0b961
Devchristian <164363325+christianlf@users.noreply.github.com>
2024-10-30 19:19:21 -0300

    100%

--- superbackend/src/prisma/prisma.ts
+++
@@ -1,6 +0,0 @@
-// src/prisma/prisma.ts
-import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient();
-
-export default prisma; // Exportando como export default
--- superbackend/src/services/ListCustomerSevice.ts
+++
@@ -1,9 +0,0 @@
-import prismaClient from "../prisma";
-
-class ListCustomerService{
-    async execute(){
-        const customer=await prismaClient.customer.findMany()
-        return customer;
-    }
-}
-export {ListCustomerService}
